682ab2aea4351c4ad5367293733ac73c15452987
Rohan Roy <b3you0870@gmail.com>
2017-12-15 00:59:40 +0530

    Change the STATIC_ROOT

--- src/petso/settings/base.py
+++ src/petso/settings/base.py
@@ -97,6 +97,19 @@ MIDDLEWARE_CLASSES = (
     # 'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
+REST_FRAMEWORK = {
+    # Use hyperlinked styles by default.
+    # Only used if the `serializer_class` attribute is not set on a view.
+    'DEFAULT_MODEL_SERIALIZER_CLASS':
+        'rest_framework.serializers.HyperlinkedModelSerializer',
+
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
